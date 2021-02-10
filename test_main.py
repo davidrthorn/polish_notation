ad8c6d3aef8a5e354e5757a7425465c2e025740c
drowthorn <drowthorn@uk.imshealth.com>
2021-02-10 15:04:33 +0000

    Add further test cases

--- test_main.py
+++ test_main.py
@@ -35,10 +35,28 @@ Additional examples
 
 
 def test_calculate_expression_with_complex_nesting_gives_correct_answer():
-    assert calculate("34567*+8910/11-12")
+    assert calculate("5 3 + 10 6 2 / - * 4 +") == 60  # (5 + 3) * (10 - (6 / 2)) + 4 == 60
+
+
+def test_calculate_expression_with_negative_number_gives_correct_answer():
+    assert calculate("-2 10 *") == -20
+
+
+def test_calculate_ignores_whitespace_padding():
+    assert calculate(" 1 2 + ") == 3
 
 
 def test_calculate_empty_string_expression_throws():  # because returning 0 would be misleading
     with pytest.raises(InvalidNotationException):
-        calculate("4+5")
+        calculate("")
 
+
+def test_calculate_expression_with_invalid_chars_throws():
+    with pytest.raises(InvalidNotationException):
+        calculate("34 + B")
+
+
+# TODO: The exception thrown here should be informative
+def test_calculate_expression_without_delimiters_throws():  # because without delimiters, notation is ambiguous
+    with pytest.raises(InvalidNotationException):
+        calculate("12-")
